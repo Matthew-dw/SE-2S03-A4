81e38e3a21482ebf65f323202e010b70ff3f4d6f
Caleb Mech <cmech1@gmail.com>
2018-11-17 17:49:17 -0500

    Moved fitness functions to their own file

---
+++ fitness.c
@@ -0,0 +1,21 @@
+#include "a4.h"
+#include <math.h>
+
+double comp_distance(const PIXEL *A, const PIXEL *B, int image_size) {
+    double d = 0.0;
+    
+    for (int i = 0; i < image_size; i++) {
+        d += pow(A[i].r - B[i].r, 2) 
+           + pow(A[i].g - B[i].g, 2) 
+           + pow(A[i].b - B[i].b, 2);
+    }
+
+    return d;
+}
+
+void comp_fitness_population(const PIXEL *image, Individual *individual, int population_size) {
+    for (int i = 0; i < population_size; i++) {
+        int image_size = individual[i].image.width * individual[i].image.height; 
+        individual[i].fitness = comp_distance(image, individual[i].image.data, image_size);
+    }
+}
--- population.c
+++ population.c
@@ -1,6 +1,5 @@
 #include "a4.h"
 #include <stdlib.h>
-#include <math.h>
 
 #define data(row, col) data[row * width + col]
 
@@ -33,24 +32,4 @@ Individual *generate_population(int population_size, int width, int height, int 
     }
 
     return population;
-}
-
-
-double comp_distance(const PIXEL *A, const PIXEL *B, int image_size) {
-    double d = 0.0;
-    
-    for (int i = 0; i < image_size; i++) {
-        d += pow(A[i].r - B[i].r, 2) 
-           + pow(A[i].g - B[i].g, 2) 
-           + pow(A[i].b - B[i].b, 2);
-    }
-
-    return d;
-}
-
-void comp_fitness_population(const PIXEL *image, Individual *individual, int population_size) {
-    for (int i = 0; i < population_size; i++) {
-        int image_size = individual[i].image.width * individual[i].image.height; 
-        individual[i].fitness = comp_distance(image, individual[i].image.data, image_size);
-    }
 }

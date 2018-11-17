d957061aedf0310d3052178cf6e4d9c91d450ba2
Caleb Mech <cmech1@gmail.com>
2018-11-17 14:37:31 -0500

    Formatting

--- population.c
+++ population.c
@@ -1,16 +1,14 @@
 #include "a4.h"
 #include <stdlib.h>
+#include <math.h>
 
 #define data(row, col) data[row * width + col]
 
-PIXEL *generate_random_image(int width, int height, int max_color)
-{
+PIXEL *generate_random_image(int width, int height, int max_color) {
     PIXEL *data = malloc(sizeof(PIXEL) * width * height);
 
-    for (int row = 0; row < height; row++)
-    {
-        for (int col = 0; col < width; col++)
-        {
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
             data(row, col).r = rand() % (max_color + 1);
             data(row, col).g = rand() % (max_color + 1);
             data(row, col).b = rand() % (max_color + 1);
@@ -20,12 +18,10 @@ PIXEL *generate_random_image(int width, int height, int max_color)
     return data;
 };
 
-Individual *generate_population(int population_size, int width, int height, int max_color)
-{
+Individual *generate_population(int population_size, int width, int height, int max_color) {
     Individual *population = malloc(sizeof(Individual) * population_size);
 
-    for (int i = 0; i < population_size; i++)
-    {
+    for (int i = 0; i < population_size; i++) {
         PPM_IMAGE *image = malloc(sizeof(PPM_IMAGE));
         image->width = width;
         image->height = height;

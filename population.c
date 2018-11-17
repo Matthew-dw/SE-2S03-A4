494a038a4ce513967ed631174db910c39020aa5c
Caleb Mech <cmech1@gmail.com>
2018-11-17 14:30:27 -0500

    Created generate_population()

--- population.c
+++ population.c
@@ -1,12 +1,16 @@
 #include "a4.h"
+#include <stdlib.h>
 
-#define data(row, col) data[row*width + col]
+#define data(row, col) data[row * width + col]
 
-PIXEL *generate_random_image(int width, int height, int max_color) {
+PIXEL *generate_random_image(int width, int height, int max_color)
+{
     PIXEL *data = malloc(sizeof(PIXEL) * width * height);
 
-    for (int row = 0; row < height; row++) {
-        for (int col = 0; col < width; col++) {
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < width; col++)
+        {
             data(row, col).r = rand() % (max_color + 1);
             data(row, col).g = rand() % (max_color + 1);
             data(row, col).b = rand() % (max_color + 1);
@@ -15,3 +19,22 @@ PIXEL *generate_random_image(int width, int height, int max_color) {
 
     return data;
 };
+
+Individual *generate_population(int population_size, int width, int height, int max_color)
+{
+    Individual *population = malloc(sizeof(Individual) * population_size);
+
+    for (int i = 0; i < population_size; i++)
+    {
+        PPM_IMAGE *image = malloc(sizeof(PPM_IMAGE));
+        image->width = width;
+        image->height = height;
+        image->max_color = max_color;
+        image->data = generate_random_image(width, height, max_color);
+
+        population[i].image = *image;
+        population[i].fitness = 0.0;
+    }
+
+    return population;
+}
